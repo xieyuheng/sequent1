01c3246a5d10a02d7ab91cd4b0727c320697ef38
XIE Yuheng <xyheme@gmail.com>
2016-04-01 14:52:57 +0800

    up

--- sequent1.scm
+++ sequent1.scm
@@ -41,7 +41,8 @@
   (case (car exp)
     [(deftype) (eva/deftype (cdr exp) env)]
     [(def) (eva/def (cdr exp) env)]
-    [(run) (eva/run (cdr exp) env)]))
+    [(def/without-check) (eva/def/without-check (cdr exp) env)]
+    [(run) (run (cdr exp) env)]))
 
 (define (eva/deftype body env)
   ;; ns -> new-ns
@@ -68,33 +69,111 @@
 
 (define (eva/def body env)
   ;; ns -> new-ns
-  (env/ns env
-          (cons (list (car body) 'function
-                      (list (cadr body))
-                      (cddr body))
-                (ns env))))
+  (define type-sequent (list (cadr body)))
+  (define data-sequent (cddr body))
+  (define new-env
+    (env/ns env
+            (cons (list (car body) 'function
+                        type-sequent
+                        data-sequent)
+                  (ns env))))
+  (define (recur1 l)
+    (if (null? l)
+      new-env
+      (if (check (car type-sequent)
+                 (car l)
+                 new-env)
+        (recur1 (cdr l))
+        #f)))
+  (recur1 data-sequent))
 
-;; (define (eva/run body env)
-;;   env)
+(define (eva/def/without-check body env)
+  ;; ns -> new-ns
+  (define type-sequent (list (cadr body)))
+  (define data-sequent (cddr body))
+  (define new-env
+    (env/ns env
+            (cons (list (car body) 'function
+                        type-sequent
+                        data-sequent)
+                  (ns env))))
+  new-env)
 
-;; (define (uni c env)
-;;   ;; ac env -> env or fail
+(define (formal-binding? e)
+  (and (list? e)
+       (not (member '-> e))))
+
+(define (formal-var? e)
+  (and (symbol? e)
+       (eq? ":" (substring (symbol->string e) 0 1))))
+
+(define (equal-var? v1 v2)
+  (and (equal? v1 v2)
+       (eq? (cadr v1) (cadr v2))))
+
+(define (arrow->arrow* arrow)
+  (define bs '())
+  (define (formal-var->var v)
+    (cond []
+          [else (list 1 (vector v))]))
+  (define (arrow->left l)
+    (cond [(eq? '-> (car l)) '()]
+          [else (cons (car l)
+                      (arrow->left (cdr l)))]))
+  (define (arrow->right arrow)
+    (cond [(eq? '-> (car l)) (cdr l)]
+          [else (arrow->right (cdr l))]))
+  (define (recur1 l)
+    (cond [(formal-var? (car l))
+           (cons (formal-var->var (car l)) )]
+          ;; [(formal-binding? (car l)) ]
+          [(list? (car l)) (recur1 (car l))]
+          [else ()]))
+  (define antecedent (recur1 (arrow->left arrow)))
+  (define succedent (recur1 (arrow->right arrow)))
+  (list antecedent succedent bs))
+
+(define (run arrow env)
+  (run* (arrow->arrow* arrow) env))
+
+(define (run* arrow* env)
+  )
+
+(define (uni c env)
+  ;; ac env -> env or fail
+  )
+
+;; (define (run/var var env)
 ;;   )
 
-;; (define (run l env)
-;;   ;; l env -> env or fail
-;;   (if (eq? '() (cdr l))
-;;     (cp (car l) env)
-;;     (())))
+;; (define (run/name ))
 
-;; (define (cp e env)
-;;   ;; e env -> env or fail
-;;   (cond [(var? e) (cp/var e env)]
-;;         [(name? e) (cp/name e env)]))
+(seq
 
-;; (define (cp/var var env)
-;;   )
+ (deftype
+   natural (-> type)
+   zero (-> natural)
+   succ (natural -> natural))
 
-;; (define (ch >< env)
-;;   ;; env -> env
-;;   )
+ (def/without-check add
+   (natural natural -> natural)
+   (:m zero -> :m)
+   (:m :n succ -> :m :n recur succ))
+
+ (def/without-check mul
+   (natural natural -> natural)
+   (:m zero -> zero)
+   (:m :n succ -> :m :n recur :m add))
+
+ (run (-> zero succ succ
+          zero succ
+          add)))
+
+(define (check type/arrow data/arrow env)
+  (check* (arrow->arrow* type/arrow)
+          (arrow->arrow* data/arrow)
+          env))
+
+(define (check* type/arrow* data/arrow* env)
+  ;; -> env or fail
+  '())
