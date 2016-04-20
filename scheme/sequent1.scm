00d8511ff0cf60563ba07ae3050f70badc1838ab
XIE Yuheng <xyheme@gmail.com>
2016-04-20 08:54:34 +0800

    scheme pass1

--- scheme/sequent1.scm
+++ scheme/sequent1.scm
@@ -110,7 +110,6 @@
       (list v default-level))))
 
 (test
-
  (list
   (parse/arrow '(natural natural -> natural))
   (parse/arrow '(natural natural -> (natural natural -> natural) natural))
@@ -121,7 +120,6 @@
   (parse/arrow '((:t^2 : type) :t -> type))
   (parse/arrow '((:t1 :t2^2 :t3^0 : j k) :t -> type))
   (parse/arrow '((:t^2 @ type) :t -> type)))
-
  '((((n natural) (n natural)) ((n natural)))
    (((n natural) (n natural)) ((a (((n natural) (n natural)) ((n natural)))) (n natural)))
    (((v (:m 0)) (n zero)) ((v (:m 0))))
@@ -131,3 +129,92 @@
    (((b (((v (:t 2))) ((n type)) #f)) (v (:t 0))) ((n type)))
    (((b (((v (:t1 1)) (v (:t2 2)) (v (:t3 0))) ((n j) (n k)) #f)) (v (:t 0))) ((n type)))
    (((b (((v (:t 2))) ((n type)) #t)) (v (:t 0))) ((n type)))))
+
+(define (pass1/arrow f s)
+  ;; formal-arrow, scope -> arrow
+  (match f
+    [(fac fsc)
+     (match (pass1/cedent fac s)
+       [(ac s0)
+        (match (pass1/cedent fsc s0)
+          [(sc s1)
+           (list ac sc)])])]))
+
+(define (pass1/cedent f s)
+  ;; formal-cedent, scope -> (cedent scope)
+  (match f
+    ['() (list '() s)]
+    [(h . r)
+     (match (pass1/dispatch h s)
+       [(v s0)
+        (match (pass1/cedent r s0)
+          [(c s1)
+           (list (cons v c) s1)])])]))
+
+(define (pass1/dispatch f s)
+  ;; formal-form, scope -> (form scope)
+  (match f
+    [('v v) (pass1/var v s)]
+    [('n n) (list (list 'name n) s)]
+    [('a a) (list (list 'arrow (pass1/arrow a s)) s)]
+    [('b b) (pass1/bind b s)]))
+
+(define (pass1/var v s)
+  ;; formal-var, scope -> (var scope)
+  (match v
+    [(symbol level)
+     (let ([found (assq symbol s)])
+       (if found
+         (let ([old (cdr found)])
+           (list (list 'var (list old level)) s))
+         (let ([new (vector symbol '())])
+           (list (list 'var (list new level))
+                 (cons (cons symbol new) s)))))]))
+
+(define (pass1/bind b s)
+  ;; formal-bind, scope -> (bind scope)
+  (match b
+    [(fvs fc leave?)
+     (match (pass1/cedent fvs s)
+       [(vs s0)
+        (match (pass1/cedent fc s0)
+          ;; this means vars in fvs can occur in fc
+          [(c s1)
+           (list (list 'bind (list vs c leave?)) s1)])])]))
+
+(test
+ (map (lambda (x) (pass1/arrow x '()))
+   (list
+    (parse/arrow '(natural natural -> natural))
+    (parse/arrow '(natural natural -> (natural natural -> natural) natural))
+    (parse/arrow '(:m zero -> :m))
+    (parse/arrow '(:m :n succ -> :m :n recur succ))
+    (parse/arrow '((:t : type) :t -> type))
+    (parse/arrow '((:t @ type) :t -> type))
+    (parse/arrow '((:t^2 : type) :t -> type))
+    (parse/arrow '((:t1 :t2^2 :t3^0 : j k) :t -> type))
+    (parse/arrow '((:t^2 @ type) :t -> type))))
+ '((((name natural) (name natural))
+    ((name natural)))
+   (((name natural) (name natural))
+    ((arrow (((name natural) (name natural))
+             ((name natural))))
+     (name natural)))
+   (((var (#(:m ()) 0)) (name zero))
+    ((var (#(:m ()) 0))))
+   (((var (#(:m ()) 0)) (var (#(:n ()) 0)) (name succ))
+    ((var (#(:m ()) 0)) (var (#(:n ()) 0)) (name recur) (name succ)))
+   (((bind (((var (#(:t ()) 1))) ((name type)) #f)) (var (#(:t ()) 0)))
+    ((name type)))
+   (((bind (((var (#(:t ()) 1))) ((name type)) #t)) (var (#(:t ()) 0)))
+    ((name type)))
+   (((bind (((var (#(:t ()) 2))) ((name type)) #f)) (var (#(:t ()) 0)))
+    ((name type)))
+   (((bind (((var (#(:t1 ()) 1)) (var (#(:t2 ()) 2)) (var (#(:t3 ()) 0))) ((name j) (name k)) #f)) (var (#(:t ()) 0)))
+    ((name type)))
+   (((bind (((var (#(:t ()) 2))) ((name type)) #t)) (var (#(:t ()) 0)))
+    ((name type)))))
+
+
+
+
