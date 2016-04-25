16fb8386f6200f99978c0e554abf4126e9bf4f9c
XIE Yuheng <xyheme@gmail.com>
2016-04-25 13:47:31 +0800

    use more passes

--- scheme/sequent1.scm
+++ scheme/sequent1.scm
@@ -2,6 +2,7 @@
 ;; using http://synthcode.com/scheme/match.scm
 (use-modules (ice-9 match))
 
+;; guile
 (define-syntax cat
   (syntax-rules ()
     [(cat (str . args))
@@ -10,6 +11,15 @@
      (string-append
       (cat (str . args))
       (cat (str2 . args2) ...))]))
+
+;; (define-syntax cat
+;;   (syntax-rules ()
+;;     [(cat (str . args))
+;;      (format str . args)]
+;;     [(cat (str . args) (str2 . args2) ...)
+;;      (string-append
+;;       (cat (str . args))
+;;       (cat (str2 . args2) ...))]))
 
 (define-syntax orz
   (syntax-rules ()
@@ -64,44 +74,73 @@
         curser
         (find-char/curser c s (+ 1 curser))))))
 
-(define (parse/arrow s)
-  ;; sexp-arrow -> formal-arrow
-  (list (parse/cedent 0 (left-of '-> s))
-        (parse/cedent 0 (right-of '-> s))))
+(define (pass1/arrow default-level s)
+  ;; default-level, form1/arrow -> form2/arrow
+  (list (pass1/cedent default-level (left-of '-> s))
+        (pass1/cedent default-level (right-of '-> s))))
 
-(define (parse/cedent default-level s)
-  ;; default-level, sexp-cedent -> formal-cedent
+(define (pass1/cedent default-level s)
+  ;; default-level, (form1 ...) -> (form2 ...)
   (match s
     ['() '()]
-    [(h . r) (cons (parse/dispatch default-level h)
-                   (parse/cedent default-level r))]))
+    [(h . r) (cons (pass1 default-level h)
+                   (pass1/cedent default-level r))]))
 
-(define (parse/dispatch default-level v)
-  ;; default-level, sexp-form -> formal-form
-  (let ([var? (lambda (v)
-                (and (symbol? v)
-                     (equal? ":" (substring (symbol->string v) 0 1))))]
-        [name? (lambda (v)
-                 (and (symbol? v)
-                      (not (eq? ":" (substring (symbol->string v) 0 1)))))]
-        [arrow? (lambda (v) (and (list? v) (member '-> v)))]
-        [im-bind? (lambda (v) (and (list? v) (member ': v)))]
-        [ex-bind? (lambda (v) (and (list? v) (member '@ v)))])
-    (cond [(var? v) (list 'v (parse/var default-level v))]
-          [(name? v) (list 'n v)]
-          [(arrow? v) (list 'a (parse/arrow v))]
-          [(im-bind? v) (list 'b
-                              (list (parse/cedent 1 (left-of ': v))
-                                    (parse/cedent 0 (right-of ': v))
-                                    #f))]
-          [(ex-bind? v) (list 'b
-                              (list (parse/cedent 1 (left-of '@ v))
-                                    (parse/cedent 0 (right-of '@ v))
-                                    #t))]
-          [else (orz ("parse/dispatch can not handle sexp-form:~a" v))])))
+(define (form1/var? v)
+  (and (symbol? v)
+       (equal? ":" (substring (symbol->string v) 0 1))))
 
-(define (parse/var default-level v)
-  ;; default-level, symbol -> formal-var
+(define (form1/name? v)
+  (and (symbol? v)
+       (not (eq? ":" (substring (symbol->string v) 0 1)))))
+
+(define (form1/arrow? v)
+  (and (list? v)
+       (member '-> v)))
+
+(define (form1/lambda? v)
+  (and (list? v)
+       (eq? (car v) 'lambda)))
+
+(define (form1/im-bind? v)
+  (and (list? v)
+       (member ': v)))
+
+(define (form1/ex-bind? v)
+  (and (list? v)
+       (member '@ v)))
+
+(define (pass1 default-level v)
+  ;; default-level, form1 -> form2
+  (cond [(form1/var? v)
+         (list 'form2/var
+               (pass1/var default-level v))]
+        [(form1/name? v)
+         (list 'form2/name
+               v)]
+        [(form1/arrow? v)
+         (list 'form2/arrow
+               (pass1/arrow default-level v))]
+        [(form1/lambda? v)
+         (list 'form2/lambda
+               (list (pass1/arrow default-level (cadr v))
+                     (map (lambda (x) (pass1/arrow default-level x))
+                       (cddr v))))]
+        [(form1/im-bind? v)
+         (list 'form2/bind
+               (list (pass1/cedent 1 (left-of ': v))
+                     (pass1/cedent 0 (right-of ': v))
+                     'leave))]
+        [(form1/ex-bind? v)
+         (list 'form2/bind
+               (list (pass1/cedent 1 (left-of '@ v))
+                     (pass1/cedent 0 (right-of '@ v))
+                     'not-leave))]
+        [else
+         (orz ("pass1 can not handle sexp-form:~a" v))]))
+
+(define (pass1/var default-level v)
+  ;; default-level, symbol -> form2/var
   (let* ([str (symbol->string v)]
          [cursor (find-char "^" str)])
     (if cursor
@@ -110,110 +149,141 @@
       (list v default-level))))
 
 (test
- (list
-  (parse/arrow '(natural natural -> natural))
-  (parse/arrow '(natural natural -> (natural natural -> natural) natural))
-  (parse/arrow '(:m zero -> :m))
-  (parse/arrow '(:m :n succ -> :m :n recur succ))
-  (parse/arrow '((:t : type) :t -> type))
-  (parse/arrow '((:t @ type) :t -> type))
-  (parse/arrow '((:t^2 : type) :t -> type))
-  (parse/arrow '((:t1 :t2^2 :t3^0 : j k) :t -> type))
-  (parse/arrow '((:t^2 @ type) :t -> type)))
- '((((n natural) (n natural)) ((n natural)))
-   (((n natural) (n natural)) ((a (((n natural) (n natural)) ((n natural)))) (n natural)))
-   (((v (:m 0)) (n zero)) ((v (:m 0))))
-   (((v (:m 0)) (v (:n 0)) (n succ)) ((v (:m 0)) (v (:n 0)) (n recur) (n succ)))
-   (((b (((v (:t 1))) ((n type)) #f)) (v (:t 0))) ((n type)))
-   (((b (((v (:t 1))) ((n type)) #t)) (v (:t 0))) ((n type)))
-   (((b (((v (:t 2))) ((n type)) #f)) (v (:t 0))) ((n type)))
-   (((b (((v (:t1 1)) (v (:t2 2)) (v (:t3 0))) ((n j) (n k)) #f)) (v (:t 0))) ((n type)))
-   (((b (((v (:t 2))) ((n type)) #t)) (v (:t 0))) ((n type)))))
+ (map (lambda (x) (pass1/arrow 0 x))
+   '((natural natural -> natural)
+     (natural natural -> (natural natural -> natural) natural)
+     (:m zero -> :m)
+     (:m :n succ -> :m :n recur succ)
+     (:m :n succ -> :m :n (lambda (natural natural -> natural)
+                            (:m :n succ -> :m :n recur succ)
+                            (:m :n succ -> :m :n recur succ)))
+     ((:t : type) :t -> type)
+     ((:t @ type) :t -> type)
+     ((:t^2 : type) :t -> type)
+     ((:t1 :t2^2 :t3^0 : j k) :t -> type)
+     ((:t^2 @ type) :t -> type)))
+ '((((form2/name natural) (form2/name natural))
+    ((form2/name natural)))
+   (((form2/name natural) (form2/name natural))
+    ((form2/arrow (((form2/name natural) (form2/name natural)) ((form2/name natural)))) (form2/name natural)))
+   (((form2/var (:m 0)) (form2/name zero))
+    ((form2/var (:m 0))))
+   (((form2/var (:m 0)) (form2/var (:n 0)) (form2/name succ))
+    ((form2/var (:m 0)) (form2/var (:n 0)) (form2/name recur) (form2/name succ)))
+   (((form2/var (:m 0)) (form2/var (:n 0)) (form2/name succ))
+    ((form2/var (:m 0)) (form2/var (:n 0)) (form2/lambda ((((form2/name natural) (form2/name natural)) ((form2/name natural))) ((((form2/var (:m 0)) (form2/var (:n 0)) (form2/name succ)) ((form2/var (:m 0)) (form2/var (:n 0)) (form2/name recur) (form2/name succ))) (((form2/var (:m 0)) (form2/var (:n 0)) (form2/name succ)) ((form2/var (:m 0)) (form2/var (:n 0)) (form2/name recur) (form2/name succ))))))))
+   (((form2/bind (((form2/var (:t 1))) ((form2/name type)) leave)) (form2/var (:t 0)))
+    ((form2/name type)))
+   (((form2/bind (((form2/var (:t 1))) ((form2/name type)) not-leave)) (form2/var (:t 0)))
+    ((form2/name type)))
+   (((form2/bind (((form2/var (:t 2))) ((form2/name type)) leave)) (form2/var (:t 0)))
+    ((form2/name type)))
+   (((form2/bind (((form2/var (:t1 1)) (form2/var (:t2 2)) (form2/var (:t3 0))) ((form2/name j) (form2/name k)) leave)) (form2/var (:t 0)))
+    ((form2/name type)))
+   (((form2/bind (((form2/var (:t 2))) ((form2/name type)) not-leave)) (form2/var (:t 0)))
+    ((form2/name type)))))
 
-(define (pass1/arrow f s)
-  ;; formal-arrow, scope -> arrow
-  (match f
-    [(fac fsc)
-     (match (pass1/cedent fac s)
-       [(ac s0)
-        (match (pass1/cedent fsc s0)
-          [(sc s1)
-           (list ac sc)])])]))
+(define (pass2/arrow a s)
+  ;; form2/arrow, scope -> (form3/arrow scope)
+  (match a
+    [(ac sc)
+     (match (pass2/cedent ac s)
+       [(3ac s1)
+        (match (pass2/cedent sc s1)
+          [(3sc s2)
+           (list (list 3ac 3sc) s2)])])]))
 
-(define (pass1/cedent f s)
-  ;; formal-cedent, scope -> (cedent scope)
-  (match f
+(define (pass2/cedent c s)
+  ;; (form2 ...), scope -> ((form3 ...) scope)
+  (match c
     ['() (list '() s)]
     [(h . r)
-     (match (pass1/dispatch h s)
-       [(v s0)
-        (match (pass1/cedent r s0)
-          [(c s1)
-           (list (cons v c) s1)])])]))
+     (match (pass2 h s)
+       [(3f s1)
+        (match (pass2/cedent r s1)
+          [(3c s2)
+           (list (cons 3f 3c) s2)])])]))
 
-(define (pass1/dispatch f s)
-  ;; formal-form, scope -> (form scope)
+(define (pass2/lambda l s)
+  ;; form2/lambda, scope -> (form3/lambda scope)
+  (match l
+    [(a al)
+     (list (list (pass2/arrow a s)
+                 (map (lambda (x) (pass2/arrow x s))
+                   al))
+           s)]))
+
+(define (pass2 f s)
+  ;; form2, scope -> (form2 scope)
   (match f
-    [('v v) (pass1/var v s)]
-    [('n n) (list (list 'name n) s)]
-    [('a a) (list (list 'arrow (pass1/arrow a s)) s)]
-    [('b b) (pass1/bind b s)]))
+    [('form2/var v)
+     (match (pass2/var v s)
+       [(v1 s1)
+        (list (list 'form3/var v1) s1)])]
+    [('form2/name n)
+     (list (list 'form3/name n) s)]
+    [('form2/arrow a)
+     (match (pass2/arrow a s)
+       [(a1 s1)
+        (list (list 'form3/arrow a1) s1)])]
+    [('form2/lambda l)
+     (match (pass2/lambda l s)
+       [(l1 s1)
+        (list (list 'form3/lambda l1) s1)])]
+    [('form2/bind b)
+     (match (pass2/bind b s)
+       [(b1 s1)
+        (list (list 'form3/bind b1) s1)])]))
 
-(define (pass1/var v s)
-  ;; formal-var, scope -> (var scope)
+(define (pass2/var v s)
+  ;; form2/var, scope -> (form3/var scope)
   (match v
     [(symbol level)
      (let ([found (assq symbol s)])
        (if found
          (let ([old (cdr found)])
-           (list (list 'var (list old level)) s))
+           (list (list old level)
+                 s))
          (let ([new (vector symbol '())])
-           (list (list 'var (list new level))
+           (list (list new level)
                  (cons (cons symbol new) s)))))]))
 
-(define (pass1/bind b s)
-  ;; formal-bind, scope -> (bind scope)
+(define (pass2/bind b s)
+  ;; form2/bind, scope -> (form3/bind scope)
   (match b
-    [(fvs fc leave?)
-     (match (pass1/cedent fvs s)
-       [(vs s0)
-        (match (pass1/cedent fc s0)
-          ;; this means vars in fvs can occur in fc
-          [(c s1)
-           (list (list 'bind (list vs c leave?)) s1)])])]))
+    [(vs c leave?)
+     (match (pass2/cedent vs s)
+       [(3vs s1)
+        (match (pass2/cedent c s1)
+          ;; this means vars in vs can occur in c
+          [(3c s2)
+           (list (list 3vs 3c leave?) s2)])])]))
 
 (test
- (map (lambda (x) (pass1/arrow x '()))
-   (list
-    (parse/arrow '(natural natural -> natural))
-    (parse/arrow '(natural natural -> (natural natural -> natural) natural))
-    (parse/arrow '(:m zero -> :m))
-    (parse/arrow '(:m :n succ -> :m :n recur succ))
-    (parse/arrow '((:t : type) :t -> type))
-    (parse/arrow '((:t @ type) :t -> type))
-    (parse/arrow '((:t^2 : type) :t -> type))
-    (parse/arrow '((:t1 :t2^2 :t3^0 : j k) :t -> type))
-    (parse/arrow '((:t^2 @ type) :t -> type))))
- '((((name natural) (name natural))
-    ((name natural)))
-   (((name natural) (name natural))
-    ((arrow (((name natural) (name natural))
-             ((name natural))))
-     (name natural)))
-   (((var (#(:m ()) 0)) (name zero))
-    ((var (#(:m ()) 0))))
-   (((var (#(:m ()) 0)) (var (#(:n ()) 0)) (name succ))
-    ((var (#(:m ()) 0)) (var (#(:n ()) 0)) (name recur) (name succ)))
-   (((bind (((var (#(:t ()) 1))) ((name type)) #f)) (var (#(:t ()) 0)))
-    ((name type)))
-   (((bind (((var (#(:t ()) 1))) ((name type)) #t)) (var (#(:t ()) 0)))
-    ((name type)))
-   (((bind (((var (#(:t ()) 2))) ((name type)) #f)) (var (#(:t ()) 0)))
-    ((name type)))
-   (((bind (((var (#(:t1 ()) 1)) (var (#(:t2 ()) 2)) (var (#(:t3 ()) 0))) ((name j) (name k)) #f)) (var (#(:t ()) 0)))
-    ((name type)))
-   (((bind (((var (#(:t ()) 2))) ((name type)) #t)) (var (#(:t ()) 0)))
-    ((name type)))))
+ (map (lambda (x) (pass2/arrow x '()))
+   (map (lambda (x) (pass1/arrow 0 x))
+     '((natural natural -> natural)
+       (natural natural -> (natural natural -> natural) natural)
+       (:m zero -> :m)
+       (:m :n succ -> :m :n recur succ)
+       (:m :n succ -> :m :n (lambda (natural natural -> natural)
+                              (:m :n succ -> :m :n recur succ)
+                              (:m :n succ -> :m :n recur succ)))
+       ((:t : type) :t -> type)
+       ((:t @ type) :t -> type)
+       ((:t^2 : type) :t -> type)
+       ((:t1 :t2^2 :t3^0 : j k) :t -> type)
+       ((:t^2 @ type) :t -> type))))
+ '(((((form3/name natural) (form3/name natural)) ((form3/name natural))) ())
+   ((((form3/name natural) (form3/name natural)) ((form3/arrow (((form3/name natural) (form3/name natural)) ((form3/name natural)))) (form3/name natural))) ())
+   ((((form3/var (#(:m ()) 0)) (form3/name zero)) ((form3/var (#(:m ()) 0)))) ((:m . #(:m ()))))
+   ((((form3/var (#(:m ()) 0)) (form3/var (#(:n ()) 0)) (form3/name succ)) ((form3/var (#(:m ()) 0)) (form3/var (#(:n ()) 0)) (form3/name recur) (form3/name succ))) ((:n . #(:n ())) (:m . #(:m ()))))
+   ((((form3/var (#(:m ()) 0)) (form3/var (#(:n ()) 0)) (form3/name succ)) ((form3/var (#(:m ()) 0)) (form3/var (#(:n ()) 0)) (form3/lambda (((((form3/name natural) (form3/name natural)) ((form3/name natural))) ((:n . #(:n ())) (:m . #(:m ())))) (((((form3/var (#(:m ()) 0)) (form3/var (#(:n ()) 0)) (form3/name succ)) ((form3/var (#(:m ()) 0)) (form3/var (#(:n ()) 0)) (form3/name recur) (form3/name succ))) ((:n . #(:n ())) (:m . #(:m ())))) ((((form3/var (#(:m ()) 0)) (form3/var (#(:n ()) 0)) (form3/name succ)) ((form3/var (#(:m ()) 0)) (form3/var (#(:n ()) 0)) (form3/name recur) (form3/name succ))) ((:n . #(:n ())) (:m . #(:m ()))))))))) ((:n . #(:n ())) (:m . #(:m ()))))
+   ((((form3/bind (((form3/var (#(:t ()) 1))) ((form3/name type)) leave)) (form3/var (#(:t ()) 0))) ((form3/name type))) ((:t . #(:t ()))))
+   ((((form3/bind (((form3/var (#(:t ()) 1))) ((form3/name type)) not-leave)) (form3/var (#(:t ()) 0))) ((form3/name type))) ((:t . #(:t ()))))
+   ((((form3/bind (((form3/var (#(:t ()) 2))) ((form3/name type)) leave)) (form3/var (#(:t ()) 0))) ((form3/name type))) ((:t . #(:t ()))))
+   ((((form3/bind (((form3/var (#(:t1 ()) 1)) (form3/var (#(:t2 ()) 2)) (form3/var (#(:t3 ()) 0))) ((form3/name j) (form3/name k)) leave)) (form3/var (#(:t ()) 0))) ((form3/name type))) ((:t . #(:t ())) (:t3 . #(:t3 ())) (:t2 . #(:t2 ())) (:t1 . #(:t1 ()))))
+   ((((form3/bind (((form3/var (#(:t ()) 2))) ((form3/name type)) not-leave)) (form3/var (#(:t ()) 0))) ((form3/name type))) ((:t . #(:t ()))))))
 
 (define (apply/arrow a e)
   ;; arrow, env -> report
