8c55cf446a6853efdac1836e689506a3adb62519
XIE Yuheng <xyheme@gmail.com>
2016-05-06 02:37:10 +0800

    - more general flower-bracket

--- flower-bracket.scm
+++ flower-bracket.scm
@@ -3,49 +3,53 @@
 ;; this error will be reported by chez as
 ;; Exception in read: more than one item found after dot (.)
 
-(define (flower-barcket/list l)
-  ;; (: list -> parsed-list)
+(define (flower-barcket f l)
+  ;; (: (list -> parsed-result) list -> parsed-list)
   (cond [(not (pair? l)) l]
         [(pair? (car l))
-         (cons (flower-barcket/list (car l))
-               (flower-barcket/list (cdr l)))]
+         (cons (flower-barcket f (car l))
+               (flower-barcket f (cdr l)))]
         [(eq? '|{| (car l))
-         (let* ([pair (flower-barcket/read (cdr l))]
+         (let* ([pair (flower-barcket/read f (cdr l))]
                 [dl (car pair)]
                 [rl (cdr pair)])
-           (cons (cons 'list dl)
-                 (flower-barcket/list rl)))]
+           (cons (f dl)
+                 (flower-barcket f rl)))]
         [else
          (cons (car l)
-               (flower-barcket/list (cdr l)))]))
+               (flower-barcket f (cdr l)))]))
 
-(define (flower-barcket/read l)
-  ;; (: list -> (readed-list . not-parsed-rest-list))
+(define (flower-barcket/read f l)
+  ;; (: (list -> parsed-result) list -> (readed-list . not-parsed-rest-list))
   (cond [(null? l) (error 'flower-barcket/read "sexp in lack of }")]
         [(eq? '|}| (car l))
          (cons '()
                (cdr l))]
         [(eq? '|{| (car l))
-         (let* ([pair (flower-barcket/read (cdr l))]
+         (let* ([pair (flower-barcket/read f (cdr l))]
                 [dl (car pair)]
                 [rl (cdr pair)]
-                [pair1 (flower-barcket/read rl)]
+                [pair1 (flower-barcket/read f rl)]
                 [dl1 (car pair1)]
                 [rl1 (cdr pair1)])
-           (cons (cons (cons 'list dl) dl1)
+           (cons (cons (f dl) dl1)
                  rl1))]
         [(pair? (car l))
-         (let* ([pair (flower-barcket/read (cdr l))]
+         (let* ([pair (flower-barcket/read f (cdr l))]
                 [dl (car pair)]
                 [rl (cdr pair)])
-           (cons (cons (flower-barcket/list (car l)) dl)
+           (cons (cons (flower-barcket f (car l)) dl)
                  rl))]
         [else
-         (let* ([pair (flower-barcket/read (cdr l))]
+         (let* ([pair (flower-barcket/read f (cdr l))]
                 [dl (car pair)]
                 [rl (cdr pair)])
            (cons (cons (car l) dl)
                  rl))]))
+
+(define (flower-barcket/list l)
+  ;; (: list -> parsed-list)
+  (flower-barcket (lambda (dl) (cons 'list dl)) l))
 
 ;; (flower-barcket/list
 ;;  '({a {a b c b {a b c} c}))
